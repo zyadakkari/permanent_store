2db53637ebe229d0c30b25484a8ae602a14c2ae5
Zyad <>
2023-01-24 11:37:02 +0000

    Added carousel to homepage, JS and CSS options

--- vendor/assets/javascripts/spree/frontend/homepage/carousel.js
+++ vendor/assets/javascripts/spree/frontend/homepage/carousel.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -11,16 +11,19 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("carousel-screen");
-    let circles = document.getElementsByClassName("circle");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < slides.length; i++) {
-        circles[i].className = slides[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    circles[slideIndex-1].className += " active";
+    let slides = Array.from(document.getElementsByClassName("carousel-screen"));
+    let circles = Array.from(document.getElementsByClassName("circle"));
+    let next = document.getElementsByClassName("next");
+    let prev = document.getElementsByClassName("prev");
+    next[0].style.display = "block";
+    prev[0].style.display = "block";
+    for (i = 0; i < slides.length; i++) {slides[i].style.display = "none";}
+    for (i = 0; i < slides.length; i++) {circles[i].className.replace(" active", "");}
+    if (n > slides.length-1 && n != 0) {slideIndex = slides.length-1}
+    if (n == 0) {prev[0].style.display = "none";}
+    if (n == slides.length-1) {next[0].style.display = "none";}
+    if (n < 0) {slideIndex = 0}
+    
+    slides[slideIndex].style.display = "block";
+    circles[slideIndex].className += " active";
 }

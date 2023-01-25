557e2d996fd4ae38ff3fb60d23afba83207d1c5f
Zyad <>
2023-01-25 13:53:36 +0000

    Added sidebar expandable submenus javascript

--- vendor/assets/javascripts/spree/frontend/homepage/sidebar.js
+++ vendor/assets/javascripts/spree/frontend/homepage/sidebar.js
@@ -1,5 +1,6 @@
 function toggleMenu(n) {
     let menus = Array.from(document.getElementsByClassName("sidebar--mobile-submenu"));
+    // let buttons = Array.from(document.getElementsByClassName("sidebar--mobile-title"));
     for(let i = 0; i < menus.length; i++) {
         if((i == n) && (!menus[i].classList.contains("is-active"))) {
             menus[i].classList.add("is-active");
@@ -9,4 +10,36 @@ function toggleMenu(n) {
             }
         }
     }
+    // for(let i = 0; i < buttons.length; i++) {
+    //     if((i == n) && (!buttons[i].classList.contains("expanded"))) {
+    //         buttons[i].classList.add("expanded");
+    //     } else {
+    //         if(buttons[i].classList.contains("expanded")) {
+    //             buttons[i].classList.remove("expanded");
+    //         }
+    //     }
+    // }
+}
+
+function toggleDesktop(n) {
+    let menus = Array.from(document.getElementsByClassName("sidebar--items"));
+    // let buttons = Array.from(document.getElementsByClassName("sidebar--title"));
+    for(let i = 0; i < menus.length; i++) {
+        if((i == n) && (!menus[i].classList.contains("is-active"))) {
+            menus[i].classList.add("is-active");
+        } else {
+            if(menus[i].classList.contains("is-active")) {
+                menus[i].classList.remove("is-active");
+            }
+        }
+    }
+    // for(let i = 0; i < buttons.length; i++) {
+    //     if((i == n) && (!buttons[i].classList.contains("expanded"))) {
+    //         buttons[i].classList.add("expanded");
+    //     } else {
+    //         if(buttons[i].classList.contains("expanded")) {
+    //             buttons[i].classList.remove("expanded");
+    //         }
+    //     }
+    // }
 }

f8a591ab88006cea49c2b0ed56fa7a85e6341798
Zyad <>
2023-01-25 10:37:35 +0000

    Added styling and javascript for mobile sidebar

--- vendor/assets/javascripts/spree/frontend/homepage/sidebar.js
+++ vendor/assets/javascripts/spree/frontend/homepage/sidebar.js
@@ -1,1 +1,12 @@
-console.log("sidebar.js loaded");
+function toggleMenu(n) {
+    let menus = Array.from(document.getElementsByClassName("sidebar--mobile-submenu"));
+    for(let i = 0; i < menus.length; i++) {
+        if((i == n) && (!menus[i].classList.contains("is-active"))) {
+            menus[i].classList.add("is-active");
+        } else {
+            if(menus[i].classList.contains("is-active")) {
+                menus[i].classList.remove("is-active");
+            }
+        }
+    }
+}
--- vendor/assets/javascripts/spree/frontend/homepage/topbar.js
+++ vendor/assets/javascripts/spree/frontend/homepage/topbar.js
@@ -1,11 +1,23 @@
 function sidebar_open() {
     var sidebar = document.getElementsByClassName("sidebar-container");
+    var main = document.getElementsByClassName("storefront-content");
+    var menu = document.getElementsByClassName("menu-sandwich");
+    var menuClose = document.getElementsByClassName("menu-close");
+    main[0].style.display = "none";
     sidebar[0].style.width = "100%";
     sidebar[0].style.display = "block";
+    menu[0].style.display = "none";
+    menuClose[0].style.display = "block";
   }
 
   function sidebar_close() {
     var sidebar = document.getElementsByClassName("sidebar-container");
+    var main = document.getElementsByClassName("storefront-content");
+    var menu = document.getElementsByClassName("menu-sandwich");
+    var menuClose = document.getElementsByClassName("menu-close");
     sidebar[0].style.display = "none";
+    main[0].style.display = "block";
+    menuClose[0].style.display = "none";
+    menu[0].style.display = "block";
     
   }

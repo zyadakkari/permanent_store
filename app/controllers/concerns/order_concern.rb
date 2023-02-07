e7f8930a66d642909edc6ff1a5ea54e6818119c5
Zyad <>
2023-02-07 08:49:37 +0000

    Fixed checkout button changing order concern and fixed search styling

--- app/controllers/concerns/order_concern.rb
+++ app/controllers/concerns/order_concern.rb
@@ -4,11 +4,15 @@ module OrderConcern
     extend ActiveSupport::Concern
 
   included do
-    helper_method :current_order
+    helper_method :current_order_bag
   end
 
-  def current_order(*args)
-    @order ||= Spree::Order.find_or_initialize_by(guest_token: cookies.signed[:guest_token], state: "cart")
+  def current_order_bag(*args)
+    if controller_name != "carts" && controller_name != "checkouts"
+      if Spree::Order.any?
+        @order_store ||= Spree::Order.find_or_initialize_by(guest_token: cookies.signed[:guest_token], state: "cart")
+      end
+    end
   end
 
 

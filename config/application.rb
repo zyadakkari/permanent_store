bb9ad35ba9358a918b9da86dfa240fd5adf020e6
ijazahmed <ijazahmed4820@gmail.com>
2023-02-11 20:09:16 +0500

    add-some-code

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module PermanentStore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+    config.active_storage.variant_processor = :vips
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

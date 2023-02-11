bee64268f11acb21f75aa4f13d5b2ec480872944
Zyad <>
2023-02-11 20:41:40 +0000

    Adding FAQ page content and fixing accordion styling

--- vendor/assets/javascripts/spree/frontend/products/accordion.js
+++ vendor/assets/javascripts/spree/frontend/products/accordion.js
@@ -1,28 +1,24 @@
 function accordionToggle(n) {
     let accordions = Array.from(document.getElementsByClassName("accordion--content"));
-    let buttons = Array.from(document.getElementsByClassName("accordion--icon"));
+    let buttons = Array.from(document.getElementsByClassName("accordion--icon"), button => button.querySelector('svg'));
     if (accordions[n].classList.contains("is-active")) {
         accordions[n].classList.remove("is-active")
-        buttons[n].querySelector(".collapse-icon").classList.remove("is-active");
-        buttons[n].querySelector(".expand-icon").classList.add("is-active");
+        buttons[n].style.transform = "rotate(90deg)";
     } else if (!accordions[n].classList.contains("is-active")) {
         accordions[n].classList.add("is-active");
-        buttons[n].querySelector(".collapse-icon").classList.add("is-active");
-        buttons[n].querySelector(".expand-icon").classList.remove("is-active");
+        buttons[n].style.transform = "rotate(180deg)";
     }
 }
 
 function mobileAccordionToggle(n) {
     let accordions = Array.from(document.getElementsByClassName("mobile--accordion--content"));
     console.log(accordions)
-    let buttons = Array.from(document.getElementsByClassName("mobile--accordion--icon"));
+    let buttons = Array.from(document.getElementsByClassName("mobile--accordion--icon"), button => button.querySelector('svg'));
     if (accordions[n].classList.contains("is-active")) {
         accordions[n].classList.remove("is-active")
-        buttons[n].querySelector(".collapse-icon").classList.remove("is-active");
-        buttons[n].querySelector(".expand-icon").classList.add("is-active");
+        buttons[n].style.transform = "rotate(90deg)";
     } else if (!accordions[n].classList.contains("is-active")) {
         accordions[n].classList.add("is-active");
-        buttons[n].querySelector(".collapse-icon").classList.add("is-active");
-        buttons[n].querySelector(".expand-icon").classList.remove("is-active");
+        buttons[n].style.transform = "rotate(180deg)";
     }
 }
